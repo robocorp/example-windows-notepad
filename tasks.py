91202efc803e76cb02f4c74b88789b3e4c16310d
Cosmin Poieana <cosmin@robocorp.com>
2024-01-22 19:35:23 +0300

    Add Windows Server 2019 support

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from datetime import datetime
 from pathlib import Path
 
@@ -12,6 +13,18 @@ window = None  # Notepad window object after being controlled
 
 LOCATOR_NOTEPAD = 'class:"Notepad" subname:"Notepad"'
 OPEN_FILE = str(os.getenv("FILE_TO_OPEN", Path("devdata") / "test.txt").absolute())
+
+
+def get_win_version() -> str:
+    """Windows only utility which returns the current Windows major version."""
+    # Windows terminal `ver` command is bugged, until that's fixed, check by build
+    #  number. (the same applies for `platform.version()`)
+    version_parts = platform.version().split(".")
+    major = version_parts[0]
+    if major == "10" and int(version_parts[2]) >= 22000:
+        major = "11"
+
+    return major
 
 
 @setup
@@ -35,20 +48,31 @@ def _select_property(unique_id: str, value: str):
 
 
 def change_font_settings():
+    ver = get_win_version()  # slightly different automation based on Windows version
+
     # Opens the Edit menu and goes into Font.
-    window.send_keys("{LAlt}e")
-    window.send_keys("{LAlt}o", wait_time=3)  # wait a while for the settings to load
+    if ver == "11":
+        window.send_keys("{LAlt}e")
+        window.send_keys("{LAlt}o", wait_time=3)  # wait a while for the settings to load
+    else:
+        window.send_keys("{LAlt}of", wait_time=1)
 
     # Now set different values for the font family, style and size.
-    _set_property("FontFamilyComboBox", "Times New Roman")
-    _select_property("FontStyleComboBox", "Regular")
-    _set_property("FontSizeComboBox", "12")
-    window.click('name:"Back"')
+    if ver == "11":
+        _set_property("FontFamilyComboBox", "Times New Roman")  # doesn't work with selection
+        _select_property("FontStyleComboBox", "Regular")
+        _set_property("FontSizeComboBox", "12")  # doesn't work with selection
+        window.click('name:"Back"')
+    else:
+        _select_property("1136", "Times New Roman")  # selection works now
+        _select_property("1137", "Regular")
+        _select_property("1138", "12")
+        desktop.send_keys("{Enter}")
 
 
 def write_text_in_editor(text: str):
     # Sets text directly inside the text editing text box of Notepad.
-    text_edit = window.find('name:"Text editor"')
+    text_edit = window.find('regex:"Text (E|e)ditor"')
     text_edit.set_value(text)
 
 
