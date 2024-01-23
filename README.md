5ecefd0b9e8b7f7bcc8afcf19bc1895fe152a8a7
Cosmin Poieana <cosmin@robocorp.com>
2024-01-23 17:19:52 +0300

    Fix env vars and README

--- README.md
+++ README.md
@@ -1,38 +1,27 @@
 # Windows Notepad automation
 
-Contains a Robocorp robot which automates Windows Notepad.
+This example demonstrates a basic Notepad app automation, compatible with both Windows
+11 and Server 2019.
 
-**Explanation.** _"Robocorp Robot"_ means a directory structure which is runnable with [RCC](https://github.com/robocorp/rcc).
+## How it works
 
-## Robot steps
-  
-  1. Open test.txt from current directory in Notepad.
-  2. Change font settings.
-  3. Clear existing text.
-  4. Write text into the editor.
-  5. Save and exit.
-  
-## Configuring run with environment variables
+1. Opens the provided [*test.txt*](devdata/test.txt) in Notepad using the Windows Run
+     widget.
+2. Initially it configures the font.
+3. Then it sets a text (current timestamp) in the already opened file.
+4. Saves the new content in the file, then closes the app.
 
-  - **FILE_TO_OPEN** (an absolute filepath to file, defaults to current directory's _test.txt_)
-  - **NOTEPAD_FONT_NAME** (default with Python is _Comic Sans MS_ and with RFW _Times New roman_)
-  - **NOTEPAD_FONT_SIZE** (default is _18_)
-  - **NOTEPAD_FONT_STYLE** (default is _Regular_)
- 
-## Running Robot with the Robot Framework syntax
+### Configuring the run with environment variables
 
-  ```shell
-  > rcc run -t "Notepad with Robot Framework"
-  ``` 
-  
-## Running Robot with the Python syntax
+- `FILE_TO_OPEN`: An absolute filepath to a file, defaults to
+ [*test.txt*](devdata/test.txt) found in *devdata*.
+- `NOTEPAD_FONT_NAME`: Defaults to "Times New Roman".
+- `NOTEPAD_FONT_STYLE`: Defaults to "Regular".
+- `NOTEPAD_FONT_SIZE`: Defaults to "12".
 
-  ```shell
-  > rcc run -t "Notepad with Python"
-  ```
-  
 ## Links
 
-  - [RCC](https://github.com/robocorp/rcc) (Github)
-  - [RCC overview](https://robocorp.com/docs/rcc/overview) (Docs)
-  - [Desktop automation](https://robocorp.com/docs/development-guide/desktop) (Docs)
+- [Windows library](https://robocorp.com/docs/python/robocorp/robocorp-windows)
+- [Windows locators](https://robocorp.com/docs/development-guide/locators/windows)
+- [Windows setup](https://robocorp.com/docs/control-room/unattended/worker-setups/windows-desktop)
+- [Desktop automation](https://robocorp.com/docs/development-guide/desktop)
--- tasks.py
+++ tasks.py
@@ -12,14 +12,17 @@ window = None  # Notepad window object after being controlled
 
 LOCATOR_NOTEPAD = 'class:"Notepad" subname:"Notepad"'
 TEXT_FILE = str(os.getenv("FILE_TO_OPEN", Path("devdata") / "test.txt").absolute())
-WINDOWS_10_VERSION = "10"
-WINDOWS_11_BUILD = 22000
+FONT_FAMILY = os.getenv("NOTEPAD_FONT_NAME", "Times New Roman")
+FONT_STYLE = os.getenv("NOTEPAD_FONT_STYLE", "Regular")
+FONT_SIZE = os.getenv("NOTEPAD_FONT_SIZE", "12")
 
 
 def get_win_version() -> str:
     """Windows only utility which returns the current Windows major version."""
     # Windows terminal `ver` command is bugged, until that's fixed, check by build
     #  number. (the same applies for `platform.version()`)
+    WINDOWS_10_VERSION = "10"
+    WINDOWS_11_BUILD = 22000
     version_parts = platform.version().split(".")
     major = version_parts[0]
     if major == WINDOWS_10_VERSION and int(version_parts[2]) >= WINDOWS_11_BUILD:
@@ -29,7 +32,7 @@ def get_win_version() -> str:
 
 
 @setup
-def open_notepad(task):
+def open_close_notepad(task):
     """Opens Notepad for automation, then closes it at the end."""
     desktop.windows_run(f"notepad.exe {TEXT_FILE}")
     yield
@@ -48,31 +51,32 @@ def _select_property(unique_id: str, value: str):
     field.select(value)
 
 
-def change_font_settings():
+def change_font_settings(family: str, style: str, size: str):
     ver = get_win_version()  # slightly different automation based on Windows version
 
     # Opens the Edit menu and goes into Font.
     if ver == "11":
         window.send_keys("{LAlt}e")
-        window.send_keys("{LAlt}o", wait_time=3)  # wait a while for the settings to load
+        window.send_keys("{LAlt}o", wait_time=3)  # wait for the settings to load
     else:
         window.send_keys("{LAlt}of", wait_time=1)
 
     # Now set different values for the font family, style and size.
     if ver == "11":
-        _set_property("FontFamilyComboBox", "Times New Roman")  # doesn't work with selection
-        _select_property("FontStyleComboBox", "Regular")
-        _set_property("FontSizeComboBox", "12")  # doesn't work with selection
-        window.click('name:"Back"')
+        _set_property("FontFamilyComboBox", family)  # doesn't work with selection
+        _select_property("FontStyleComboBox", style)
+        _set_property("FontSizeComboBox", size)  # doesn't work with selection
+        window.click('name:"Back"')  # goes back and automatically saves the settings
     else:
-        _select_property("1136", "Times New Roman")  # selection works now
-        _select_property("1137", "Regular")
-        _select_property("1138", "12")
-        desktop.send_keys("{Enter}")
+        # Selection works everywhere now.
+        _select_property("1136", family)
+        _select_property("1137", style)
+        _select_property("1138", size)
+        desktop.send_keys("{Enter}")  # saves the new settings
 
 
 def write_text_in_editor(text: str):
-    # Sets text directly inside the text editing text box of Notepad.
+    # Sets text directly inside the text editing box of Notepad.
     text_edit = window.find('regex:"Text (E|e)ditor"')
     text_edit.set_value(text)
 
@@ -82,6 +86,6 @@ def automate_notepad():
     """Writes some text in Notepad after changing the font."""
     global window
     window = windows.find_window(LOCATOR_NOTEPAD)
-    change_font_settings()
+    change_font_settings(FONT_FAMILY, FONT_STYLE, FONT_SIZE)
     write_text_in_editor(f"Timestamp: {datetime.now()}")
-    window.send_keys("{Ctrl}s")
+    window.send_keys("{Ctrl}s")  # saves the new content
